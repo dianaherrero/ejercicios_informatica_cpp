3350a982160426dff9d6e134ba0806ef91458863
dianaherrero <45766177+dianaherrero@users.noreply.github.com>
2019-03-01 19:08:44 +0100

    01/03

---
+++ mem_dinamica_matriz/mem_dinamica_matriz.cpp
@@ -0,0 +1,16 @@
+
+#include "pch.h"
+#include <iostream>
+
+#include <string>
+
+int main() {
+	std::string str = "mi abuela";
+	std::string str1 = str;
+
+	std::cout << str1;
+
+	std::cout << "longitud: " << str.size();
+
+
+}
